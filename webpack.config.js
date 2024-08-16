3c296bbfc50427a58e10693e6a0cb1305e72d4c4
竹下陽喜 <>
2024-08-16 17:34:52 +0900

    一通り完成

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const JavaScriptObfuscator = require('webpack-obfuscator');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
   entry: './src/router/index.js',
@@ -14,6 +15,7 @@ module.exports = {
     minimizer: [new TerserPlugin()],
   },
   plugins: [
+    new VueLoaderPlugin(), // VueLoaderPlugin を追加
     new JavaScriptObfuscator({
       rotateStringArray: true,
     }, ['excluded_bundle_name.js'])
@@ -30,6 +32,23 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.vue$/, // .vue ファイルを処理するためのルールを追加
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.css$/, // CSS ファイルを処理するルールを追加（必要な場合）
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
     ],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.js', '.vue', '.json'], // 拡張子の解決を追加
   },
 };
