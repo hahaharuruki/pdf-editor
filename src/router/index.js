08e4c1e697050d6194fa61b5c4fc6d288facf2e3
竹下陽喜 <>
2024-08-29 11:41:37 +0900

    リダイレクトの修正

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import PdfToImage from '../views/PdfToImage.vue';
-import MergePdf from '../views/MergePdf.vue';
-import PdfCompress from '../views/PdfCompress.vue';
-import SplitPdf from '../views/SplitPdf.vue';
-import ImageToPdf from '../views/ImageToPdf.vue';
-import WatermarkPdf from '../views/WatermarkPdf.vue';
-import RotatePdf from '../views/RotatePdf.vue';
-import PageNumberPdf from '../views/PageNumberPdf.vue';
-// import SignPdf from '../views/SignPdf.vue';
-import PrivacyPolicy from '@/views/PrivacyPolicy.vue';
-import FaqPage from '@/views/FaqPage.vue';
-import ContactPage from '@/views/ContactPage.vue';
-import BlogPage from '../views/BlogPage.vue';
-// import HowToUseMergePdf from '../blog/HowToUseMergePdf.vue';
+
 
 const router = createRouter({
   history: createWebHistory(),
