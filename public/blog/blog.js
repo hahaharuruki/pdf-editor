f145304c52537cb46efa9b18027cfd0791b941ad
竹下陽喜 <>
2024-09-10 20:55:57 +0900

    mapの構築

---
+++ public/blog/blog.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const tocHeader = document.querySelector(".toc-header");
+  const tocList = document.querySelector(".toc ul");
+  const tocArrow = tocHeader.querySelector("span");
+
+  // 目次の開閉機能
+  tocHeader.addEventListener("click", function() {
+      if (tocList.style.display === "none" || tocList.style.display === "") {
+          tocList.style.display = "block";
+          tocArrow.textContent = "▲"; // 展開時に矢印を上向きに変更
+      } else {
+          tocList.style.display = "none";
+          tocArrow.textContent = "▼"; // 非表示時に矢印を下向きに変更
+      }
+  });
+
+  // 初期状態で目次を非表示に
+  tocList.style.display = "none";
+  tocArrow.textContent = "▼"; // 初期状態の矢印を下向きに
+});
+
+window.addEventListener('load', function() {
+  var sidebar = document.querySelector('.sidebar');
+  var pageHeight = document.body.scrollHeight;
+  sidebar.style.height = pageHeight + 'px';
+});
+
+window.addEventListener('resize', function() {
+  var sidebar = document.querySelector('.sidebar');
+  var pageHeight = document.body.scrollHeight;
+  sidebar.style.height = pageHeight + 'px';
+});
